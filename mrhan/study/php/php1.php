7bc1b3a8304022ca77e92852fde4401fb6a1ed5d
hanjinbao <hanjinbao@huanqiu.com>
2018-11-09 09:13:21 +0800

    php1

---
+++ mrhan/study/php/php1.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="x-ua-compatible" content="ie=7" />
+
+<title>PHP</title>
+
+<meta http-equiv="Access-Control-Allow-Origin" content="*" />
+
+<!-- build:css css/main.min.css -->
+<link rel="stylesheet" type="text/css" href="http://topic.visitbeijing.com.cn/site/2018/S/lbh/css/baseZt.css"/>
+<link rel="stylesheet" type="text/css" href="http://topic.visitbeijing.com.cn/site/2018/S/lbh/css/index.css">
+<!-- endbuild -->
+
+</head>
+<body>
+
+<?php
+echo "我的第一段 PHP 脚本！";
+?>  
+
+
+
+
+</body>
+
+</html>
+
+
